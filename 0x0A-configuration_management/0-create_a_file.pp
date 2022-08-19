65e655cc09c71499bc93b0240241e177acc553a9
Dontcode44 <4659@holbertonschool.com>
2022-08-19 18:23:17 -0500

    Create

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Init of the Puppet
 
 file { '/tmp/school':
-  path    => '/tmp/school'
+  path    => '/tmp/school',
   owner   => 'www-data',
   group   => 'www-data',
   mode    => '0744',
-  content => 'I love Puppet',
+  content => 'I love Puppet'
 }
