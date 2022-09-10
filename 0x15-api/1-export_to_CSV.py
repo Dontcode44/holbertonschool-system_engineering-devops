88300750feacbe4005e6a81a2911aa77250432c0
Dontcode44 <kabutto378@gmail.com>
2022-09-10 15:37:30 -0500

    Add - Export a csv👾

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Using the rest API"""
+
+import requests
+import sys
+import csv
+
+if __name__ == "__main__":
+    """
+    Write a Python script that, using this REST API
+    """
+    emp_id = int(sys.argv[1])
+
+    users = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                         .format(emp_id)).json()
+
+    u_all = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
+                         .format(emp_id)).json()
+
+    with open("{}.csv".format(emp_id), 'w') as fi_csv:
+        f_ile = fi_csv.writer(fi_csv, delimiter=',', quoting=csv.QUOTE_ALL)
+        for task in u_all:
+            f_ile.writerow([emp_id, users.get('username'),
+                            task.get('completed'), task.get('title')])
