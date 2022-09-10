64cb5041768516f9e89f4b8fc78da15ff0894600
Dontcode44 <kabutto378@gmail.com>
2022-09-10 16:02:34 -0500

    Fixed - Export a csv👾

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
     Write a Python script that, using this REST API
     """
     emp_id = int(sys.argv[1])
+    file_csv = sys.argv[1] + '.csv'
 
     users = requests.get("https://jsonplaceholder.typicode.com/users/{}"
                          .format(emp_id)).json()
@@ -17,8 +18,13 @@ if __name__ == "__main__":
     u_all = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
                          .format(emp_id)).json()
 
-    with open("{}.csv".format(emp_id), 'w') as fi_csv:
-        f_ile = csv.writer(fi_csv, delimiter=',', quoting=csv.QUOTE_ALL)
-        for task in u_all:
-            f_ile.writerow([emp_id, users.get('username'),
-                           task.get('completed'), task.get('title')])
+    with open(file_csv, mode='w') as csv_file:
+        the_file = csv.writer(csv_file, delimiter=',', quotechar='"',
+                              quoting=csv.QUOTE_ALL)
+        for x in users:
+            if emp_id == x.get("id"):
+                user_name = (x.get("username"))
+        for y in u_all:
+            if y.get("userId") == emp_id:
+                the_file.writerow([str(emp_id), user_name, y.get("completed"),
+                                   y.get("title")])
